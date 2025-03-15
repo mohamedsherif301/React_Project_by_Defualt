cbd59959d49e305ad3d2b898b2303bee4792444d
mohamedsherif301 <myosre122@gmail.com>
2025-03-15 11:06:22 +0200

    Ahmed Ibrahim Feature

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -3,8 +3,12 @@ import React from "react";
 
 
 class Welcome extends React.Component{
+  function  Ahmedc(params) {
+    const x=0;
+    return 5;
+  }
     render(){
-    // ahmed feature 
+       
       
     }
 }
